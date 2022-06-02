417f50a776c85322a9be1a0d3c093a954399ccd8
Alexis <alexis.un16@gmail.com>
2022-06-02 10:53:24 -0500

    respaldo12

---
+++ Sesión 17/js/JavaScript.js
@@ -0,0 +1,19 @@
+function compara() { 
+
+    var a=2;
+    var b=3;
+    if (a==b)
+    alert ("a y b son iguales");
+    else
+    alert("a y b son diferentes"); 
+      
+    }
+function resta(x,y){
+    var rest =x-y;
+    document.write("<h2> la resta es:"+rest+"</h2>");
+}
+function suma(x,y){
+    var rest =x+y;
+    document.getElementById("sumar").innerHTML-"suma es: "+sum;
+}
+
